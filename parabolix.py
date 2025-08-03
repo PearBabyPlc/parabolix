90e51ee5555906ff8eff8cf1b2f17a0e2a43c5a7
PearBaby <38026571+PearBabyPlc@users.noreply.github.com>
2025-08-03 23:05:29 +1000

    Update parabolix.py

--- parabolix.py
+++ parabolix.py
@@ -1,4 +1,6 @@
+import math
 import matplotlib.pyplot as plt
+import scipy.integrate as integrate
 import geometry as geom
 import formulaegg as egg
 import parser
@@ -7,7 +9,7 @@ plt.style.use('dark_background')
 
 designParams = {
     "Rt": 0.1,
-    "expR": 200,
+    "expR": 130,
     "conR": 3,
     "expA": 30,
     "exitA": 5,
@@ -19,6 +21,15 @@ designParams = {
 egg.printSpacer()
 geometry, actualParams = geom.getGeometry(designParams)
 geomX, geomY, mirrX, mirrY = geometry
+positiveGeom = dict(zip(geomX, geomY))
+injectorX = actualParams.get("injectorX")
+combEndX = actualParams.get("combEndX")
+injectorY = positiveGeom.get(injectorX)
+combEndY = positiveGeom.get(combEndX)
+injectorLineX = (injectorX, injectorX)
+injectorLineY = (injectorY, -injectorY)
+combEndLineX = (combEndX, combEndX)
+combEndLineY = (combEndY, -combEndY)
 
 egg.printSpacer()
 print("Parsed CEARUN data:")
@@ -26,12 +37,70 @@ positions = parser.parseCEARUN() #file MUST be named "cearun.txt"
 for posEntry in positions:
     print(posEntry.summary())
 
-egg.printSpacer()
+#egg.printSpacer()
+#print("Verbose CEARUN data:")
+#for posEntry in positions:
+#    print()
+#    print(posEntry)
 
-fig, ax = plt.subplots()
-ax.plot(geomX, geomY)
-ax.plot(mirrX, mirrY)
+egg.printSpacer()
+designWallTemp = 1500
+stressesAndFluxes = egg.getStressesAndFluxes(positiveGeom, actualParams, positions, designWallTemp)
+plotX = []
+plotP = []
+plotT = []
+plotQ = []
+listA = []
+for entry in stressesAndFluxes.items():
+    x = entry[0]
+    ptq = entry[1]
+    plotX.append(x)
+    pBar = ptq[0] #not actually bar
+    plotP.append(pBar)
+    plotT.append(ptq[1])
+    plotQ.append(ptq[2])
+    listA.append(ptq[-1])
+    print(x, pBar, ptq[1], ptq[2])
+
+plotXzero = []
+for x in plotX:
+    plotXzero.append(0)
+
+def getTotalHeating(xarray, ylist, aList):
+    it = -1
+    yarray = []
+    for a in aList:
+        it += 1
+        r = math.sqrt(a / math.pi)
+        c = math.pi * 2 * r
+        y = ylist[it] * c
+        yarray.append(y)
+    return integrate.trapezoid(yarray, xarray)
+
+totalHeating = getTotalHeating(plotX, plotQ, listA)
+print("TOTAL HEATING =", totalHeating, "WATTS")
+        
+
+fig, (bx, ax) = plt.subplots(2, 1, sharex='col')
+ax.plot(geomX, geomY, color='white')
+ax.plot(mirrX, mirrY, color='white')
+ax.plot(injectorLineX, injectorLineY, linestyle='dashed', color='red')
+ax.plot(combEndLineX, combEndLineY, linestyle='dotted', color='yellow')
 ax.grid()
 plt.gca().set_aspect('equal')
+px = ax.twinx()
+px.plot(plotX, plotP, color='aqua')
+px.set_ylabel('Pressure (Pa)')
+#px.set_yscale('log')
+bx.plot(plotX, plotT, color='yellow')
+bx.set_ylabel('Temperature (K)')
+bx.scatter(plotX, plotXzero, marker='*')
+qx = bx.twinx()
+qx.plot(plotX, plotQ, color='firebrick')
+qx.set_ylabel('Heat flux (W/sq m)')
+qx.set_yscale('log')
+px.grid(linestyle='dashed')
+bx.grid()
+qx.grid(linestyle='dashed')
 plt.savefig("parabolix.pdf")
 plt.show()
